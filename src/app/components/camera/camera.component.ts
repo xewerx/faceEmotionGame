8a072f503f5276613ded44a2c91e848e5d65f7a2
Ewaryst Ławecki <ewaryst@spoko.pl>
2020-08-30 21:10:09 +0200

    components layout, camera, face-api

--- src/app/components/camera/camera.component.ts
+++ src/app/components/camera/camera.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import * as faceapi from 'face-api.js';
 
 @Component({
   selector: 'app-camera',
@@ -7,9 +8,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CameraComponent implements OnInit {
 
-  constructor() { }
+  emotions = {
+    neutral: 'Neutralny 😐',
+    surprised: 'Zaskoczony 😮',
+    disgusted: 'Zniesmaczony',
+    fearful: 'Wystraszony 😨',
+    sad: 'Smutny 🙁',
+    angry: 'Zły 😠',
+    happy: 'Wesoły 😃',
+};
+    video: any;
+    canvas: any;
+    result: any;
+    displaySize: any;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.canvas = document.querySelector('.js-overlay');
+    this.result = document.querySelector('.js-emotion');
+    this.video = document.querySelector('.js-video');
+    this.displaySize = { width: '850', height: '550' };
+    console.log(this.video);
+
+    // this.startVideo();
+
+    Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri('assets'),
+      faceapi.nets.faceExpressionNet.loadFromUri('assets')
+     ]).then(() => this.startVideo(this.video)).catch(() => console.log('nie dziala'));
+
+   }
+
+  startVideo(videoStream): void {
+
+    navigator.getUserMedia({ video: true },
+        (stream) => {
+          videoStream.srcObject = stream;
+          this.detectFace();
+          },
+          (err) => {
+            return console.error(err);
+          }
+      );
+
+
+
   }
+
+  showExpression({ expressions }) {
+    const arr = Object.entries(expressions);
+    const max = arr.reduce((acc, current) => {
+      return acc[1] > current[1] ? acc : current;
+    }, []);
+    this.result.textContent = this.emotions[max[0]];
+  }
+
+  async detectFace() {
+
+    const options = new faceapi.TinyFaceDetectorOptions();
+    faceapi.matchDimensions(this.canvas, this.displaySize);
+
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(this.video, options).withFaceExpressions();
+      const resizedDetections = faceapi.resizeResults(detections, this.displaySize);
+      this.canvas.getContext('2d').clearRect(0, 0, this.canvas.width, this.canvas.height);
+      faceapi.draw.drawDetections(this.canvas, resizedDetections);
+
+      if (detections[0]) {
+        this.showExpression(detections[0]);
+      }
+    }, 100);
+
+  }
+
+
+
+
+
+
+
+
+
 
 }
