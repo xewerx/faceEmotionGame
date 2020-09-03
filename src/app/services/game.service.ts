196965f604a5619013856d90b86b6b189c6bb0ec
Ewaryst Ławecki <ewaryst@spoko.pl>
2020-09-03 21:45:13 +0200

    game panel

---
+++ src/app/services/game.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  constructor() { }
+
+  private valueProgressBar = 0;
+  private valueProgressBarSum = new Subject<number>();
+
+  loading(difference): void {
+    const action = setInterval(() => {
+      console.log('DZIALA');
+      // tslint:disable-next-line: max-line-length
+      if (this.valueProgressBar >= 100) { clearInterval(action); } else { this.valueProgressBarSum.next(this.valueProgressBar += difference); }
+    }, 50);
+  }
+
+  getValue(): Observable<number> {
+    return this.valueProgressBarSum.asObservable();
+  }
+
+}
